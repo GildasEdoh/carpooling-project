60a066dd7316e1939e51f10abff84f756d8c4f03
GildasEdoh <edohgildas323@gmail.com>
2025-01-15 18:22:34 +0000

    database connection

--- app/src/main/java/tg/crsandroid/carpool/MainActivity.kt
+++ app/src/main/java/tg/crsandroid/carpool/MainActivity.kt
@@ -1,6 +1,7 @@
 package tg.crsandroid.carpool
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,9 +12,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import tg.crsandroid.carpool.ui.theme.CarpoolTheme
 
 class MainActivity : ComponentActivity() {
+    val db = Firebase.firestore
+    val TAG = "BASE Firestore"
+    val user = hashMapOf(
+        "first" to "ada",
+        "last" to "lovelace",
+        "born" to 2015,
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,6 +37,32 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        db.collection("users-test").add(user)
+            .addOnSuccessListener{ documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+
+
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+        // Create a new user with a first, middle, and last name
+        val user2 = hashMapOf(
+            "first" to "Alan",
+            "middle" to "Mathison",
+            "last" to "Turing",
+            "born" to 1912,
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users-test")
+            .add(user2)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
     }
 }
 
